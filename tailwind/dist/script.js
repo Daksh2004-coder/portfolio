46453c055af1da73a2ed76fb928aba4dbc59ae91
daksh <dakshnanoma912gmail.com>
2025-03-06 23:46:28 +0530

    update-nav-bar

--- tailwind/dist/script.js
+++ tailwind/dist/script.js
@@ -11,6 +11,25 @@ button.addEventListener("click", function(){
 })
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".scroll-link").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent default anchor behavior
+
+            let targetId = this.getAttribute("href").substring(1);
+            let targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.querySelectorAll("#nav_bar a");
